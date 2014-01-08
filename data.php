cb252e1138b41b3b92cf32aa2b1f50dc124a6482
X16 <x16.tux@gmail.com>
2014-01-07 19:32:43 -0500

    Fixed query

--- data.php
+++ data.php
@@ -2,12 +2,11 @@
 	$o    = $_GET['o'];
 	$temp = $_GET['TEMP'];
 
-	$sql = mysql_query("INSERT INTO 'temperature' VALUES('$temp')");
 
 	$con = mysql_connect("localhost", "arduinot", "6AApjDjf") or die(mysql_error());
 	mysql_select_db("arduinot") or die(mysql_error());
 
-	if (!mysql_query($sql, $con)) {
+	if (!mysql_query("INSERT INTO 'temperature' VALUES('$temp')")) {
 		die('Error: ' . mysql_error());
 	}
 	
