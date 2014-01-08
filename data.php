7e4f7ee9a657dbe9bae4ead22573632a106744dd
X16 <x16.tux@gmail.com>
2014-01-07 19:28:37 -0500

    fixed to add data to table

--- data.php
+++ data.php
@@ -6,6 +6,10 @@
 
 	$con = mysql_connect("localhost", "arduinot", "6AApjDjf") or die(mysql_error());
 	mysql_select_db("arduinot") or die(mysql_error());
+
+	if (!mysql_query($sql, $con)) {
+		die('Error: ' . mysql_error());
+	}
 	
 	echo "OK";
 ?>
