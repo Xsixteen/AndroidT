183466ae0937679777dfa1067d91087d526ec126
X16 <x16.tux@gmail.com>
2014-01-07 19:16:43 -0500

    code update

--- data.php
+++ data.php
@@ -1,9 +1,11 @@
 <?php
+	$o    = $_GET['o'];
 	$temp = $_GET['TEMP'];
-	
-	
 
+	$sql = mysql_query("INSERT INTO 'temperature' VALUES('$temp')");
+
+	$con = mysql_connect("localhost", "arduinot", "6AApjDjf") or die(mysql_error());
+	mysql_select_db("arduinot") or die(mysql_error());
 	
-
-
+	echo "OK";
 ?>
