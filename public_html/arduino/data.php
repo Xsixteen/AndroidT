4604a051cfdde609b375f6fa1e0964f03a128a2b
X16 <x16.tux@gmail.com>
2014-01-23 22:30:32 -0500

    Modified big chart to only be 2 week range

--- public_html/arduino/data.php
+++ public_html/arduino/data.php
@@ -13,7 +13,9 @@
 	mysql_select_db("arduinot") or die(mysql_error());
 
 	if($o == "ts") {
-		$result = mysql_query("SELECT Time FROM temperature", $con) or die('Error: ' . mysql_error());
+		$now      = date("Y-m-d H:m:s", strtotime("now"));
+		$twoweeks = date("Y-m-d H:m:s", strtotime("-2 weeks"));
+		$result = mysql_query("SELECT Time FROM temperature WHERE Time <= '$now' AND Time >= '$twoweeks'", $con) or die('Error: ' . mysql_error());
 		while($row = mysql_fetch_assoc($result)) {
 			array_push($stack, date('n-j \a\t ga',strtotime($row["Time"])));
 		}
@@ -32,7 +34,9 @@
 		echo json_encode($stack);
 	} else if(isset($o)) {
 		//Handle read request
-		$result = mysql_query("SELECT * FROM temperature", $con) or die('Error: ' . mysql_error());
+		$now      = date("Y-m-d H:m:s", strtotime("now"));
+		$twoweeks = date("Y-m-d H:m:s", strtotime("-2 weeks"));
+		$result = mysql_query("SELECT * FROM temperature WHERE Time <= '$now' AND Time >= '$twoweeks'", $con) or die('Error: ' . mysql_error());
 		while($row = mysql_fetch_assoc($result)) {
 			array_push($stack, $row);
 		}
